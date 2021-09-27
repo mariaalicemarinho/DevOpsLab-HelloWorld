7dbb658edcd639e4b729a9374c788423e87bd01e
Maria Alice <mariaalicemarinho3@gmail.com>
2021-09-27 15:33:08 -0400

    Alterando o cabeçalho

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Pipeline DvOps) - Maria Alice Marinho.")
+        self.assertRegex(result.data.decode(), "Pipeline DevOps - Maria Alice Marinho")
 
 
 if __name__ == "__main__":
